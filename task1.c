1995a9d7f16ded2d5b390a0d0e3004cf7bc492aa
devirsaint <delexxragezzero@mail.ru>
2025-11-11 10:16:00 +0300

    Update task1.c

--- task1.c
+++ task1.c
@@ -1,10 +1,8 @@
-/*Вариант 14*/
+/* вариант 14 */
 
 #include <stdio.h>
 #include <math.h>
 
-
-/* Объявляем экспоненту*/
 #ifndef EXP
 #define EXP 2.718
 #endif
@@ -17,7 +15,6 @@
  * @param z - константа z
  * 
 */
-
 double get_sol_a(double x, double y, double z);
 double get_sol_b(double x, double y, double z);
 
@@ -52,14 +49,11 @@ int main(void)
 
 double get_sol_a(double x, double y, double z)
 {
-    double a1 = (double)pow(z, 2) * x + (double)pow(EXP, x * -1) * cos(y * x);
-    double a2 = y * x - (double)pow(EXP, x * -1) * sin(y * x) + 1;
-    double a = a1 / a2;
-    return a;
+    return ((double)pow(z, 2) * x + (double)pow(EXP, x * -1) * cos(y * x)) / (y * x - (double)pow(EXP, x * -1) * sin(y * x) + 1);
 }
 
 /**
- * @brief функцыя для вычисления B
+ * @brief функция для вычисления B
  * @param x - константа x
  * @param y - константа y
  * @param z - константа z
@@ -67,6 +61,5 @@ double get_sol_a(double x, double y, double z)
 
 double get_sol_b(double x, double y, double z)
 {
-    double b = (double)exp(2 * x) * log(z + x) - (double)pow(y, 3 * x) * log(y - x);
-    return b;
+    return (double)exp(2 * x) * log(z + x) - (double)pow(y, 3 * x) * log(y - x);
 }
