8cf8737fa4fd7415ad3e9a55a4fb464d861439e1
devirsaint <delexxragezzero@mail.ru>
2025-11-24 12:16:04 +0300

    Update task1.c

--- task1.c
+++ task1.c
@@ -6,26 +6,28 @@
 #endif
 
 /**
- * @brief прототипы функции А
- * @param x - константа x
- * @param y - константа y
- * @param z - константа z
+ * @brief прототип функции А, рассчитывает первую формулу
+ * @param x - константа x (первое число)
+ * @param y - константа y (второе число)
+ * @param z - константа z (третье число)
  */
 double get_sol_a(double x, double y, double z);
 /**
- * @brief прототипы функции B
- * @param x - константа x
- * @param y - константа y
- * @param z - константа z
+ * @brief прототип функции B, рассчитывает вторую формулу
+ * @param x - константа x (первое число)
+ * @param y - константа y (второе число)
+ * @param z - константа z (третье число)
  */
 double get_sol_b(double x, double y, double z);
 
 /**
  * @brief точка входа в программу
+ * @return Возвращает 0 если все работает
  */
 int main(void) 
 {
     /*объявляем константы*/
+
     const double x = 0.3;
     const double y = 2.9;
     const double z = 0.5;
@@ -36,23 +38,11 @@ int main(void)
     return 0;
 }
 
-/**
- * @brief функция для вычисления A
- * @param x - константа x
- * @param y - константа y
- * @param z - константа z
- */
 double get_sol_a(double x, double y, double z)
 {
     return ((double)pow(z, 2) * x + (double)pow(EXP, x * -1) * cos(y * x)) / (y * x - (double)pow(EXP, x * -1) * sin(y * x) + 1);
 }
 
-/**
- * @brief функция для вычисления B
- * @param x - константа x
- * @param y - константа y
- * @param z - константа z
- */
 double get_sol_b(double x, double y, double z)
 {
     return (double)exp(2 * x) * log(z + x) - (double)pow(y, 3 * x) * log(y - x);
