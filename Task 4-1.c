6820c8e3cfac3b577d4b459e06705fb24e09851c
Alexey Kopysov <delexxragezzero@mail.ru>
2025-12-29 14:26:10 +0300

    Update Task 4-1.c

--- Task 4-1.c
+++ Task 4-1.c
@@ -21,21 +21,21 @@ void is_array(const int *arr);
  * @param len Длина массива
  * @param arr Массив, который заполняет
  */
-void fill_manual(const int len, int *array);
+void fill_manual(const size_t len, int *array);
 
 /**
  * @brief Заполняет массив случаныйми элементами из заданного промежутка
  * @param len Длина массива
  * @param arr Массив, который заполняет
  */
-void fill_random(const int len, int *array);
+void fill_random(const size_t len, int *array);
 
 /**
  * @brief Функция вывода массива
  * @param arr Массив
  * @param len Длина массива
  */
-void print_arr(const int *arr, int len);
+void print_arr(const int *arr, const size_t len);
 
 /**
  * @brief Ищет сумму элементов, значения которых состоит из одной цифры
@@ -43,7 +43,7 @@ void print_arr(const int *arr, int len);
  * @param len Длина массива
  * @return Возвращает сумму элементов
  */
-int sum_1dig(const int *arr, int len);
+int sum_1dig(const int *arr, const size_t len);
 
 /**
  * @brief Функция копирует массив
@@ -58,7 +58,7 @@ void copy_paste_arr(int *copied_arr, const int *pasted_arr, const size_t len);
  * @param arr Массив
  * @param len Длина массива
  */
-void reverse_arr(int *arr, int len);
+void reverse_arr(int *arr, const size_t len);
 
 /**
  * @brief Ищет номер последней пары соседних элементов с одинаковыми знаками, произведение которых меньше заданного числа
@@ -77,7 +77,7 @@ int main(void) {
 
     printf("Количество элементов в массиве:\n");
 
-    int len = get_val();
+    size_t len = get_val();
 
     if (len <= 0) {
         fprintf(stderr, "Error: кол-во элементов <= 0, должно быть > 0");
@@ -154,15 +154,15 @@ void is_array(const int *arr) {
   }
 }
 
-void fill_manual(const int len, int *array) {
+void fill_manual(const size_t len, int *array) {
     printf("Введите каждый элемент массива (%d):\n", len);
-    for (int i = 0; i < len; i++) {
+    for (size_t i = 0; i < len; i++) {
         printf("Элемент %d: ", i);
         array[i] = get_val();
     }
 }
 
-void fill_random(const int len, int *arr) {
+void fill_random(const size_t len, int *arr) {
     printf("\nДиапазон для случайных чисел\n");
 
     printf("\nМинимальное значение:\n");
@@ -183,18 +183,18 @@ void fill_random(const int len, int *arr) {
     }
 }
 
-void print_arr(const int *arr, int len) {
+void print_arr(const int *arr, const size_t len) {
     printf("[");
     if (len > 0) {
         printf("%d", arr[0]);
-        for (int i = 1; i < len; i++) {
+        for (size_t i = 1; i < len; i++) {
             printf(", %d", arr[i]);
         }
     }
     printf("]\n");
 }
 
-int sum_1dig(const int *arr, int len) {
+int sum_1dig(const int *arr, const size_t len) {
     int sum = 0;
     for (int i = 0; i < len; i++) {
         int num = arr[i];
@@ -207,14 +207,14 @@ int sum_1dig(const int *arr, int len) {
     return sum;
 }
 
-int last_pair(const int *arr, const size_t length) {
+int last_pair(const int *arr, const size_t len) {
   printf("Введите число, по которому будет производиться поиск пары\n");
   int num = get_val();
   if (num < 0) {
     fprintf(stderr, "Введеное число < 0, должно быть > 0\n");
     exit(1);
   }
-  for (size_t i = length - 1; i > 0; i--) {
+  for (size_t i = len - 1; i > 0; i--) {
     if ((arr[i] * arr[i - 1] < num) &&
         ((arr[i] < 0 && arr[i - 1] < 0) || (arr[i] > 0 && arr[i - 1] > 0))) {
       return i;
@@ -230,7 +230,7 @@ void copy_paste_arr(int *copied_arr, const int *pasted_arr, const size_t len) {
   }
 }
 
-void reverse_arr(int *arr, int len) {
+void reverse_arr(int *arr, const size_t len) {
     if (len < 3) {
         fprintf(stderr, "В массиве недостаточно элементов для замены элементов");
         exit(1);
@@ -242,7 +242,7 @@ void reverse_arr(int *arr, int len) {
     int min_i = 0;
     int max_i = 0;
 
-    for (int i = 1; i < len; i++) {
+    for (size_t i = 1; i < len; i++) {
         if (arr[i] < arr[min_i]) {
             min_i = i;
         }
